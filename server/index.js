55212c0308fc8088311362729773162d5645b84b
cdmccune <curtmccune@gmail.com>
2022-04-11 15:40:50 -0700

    added functionality on server side with buttons for post and delete

--- server/index.js
+++ server/index.js
@@ -4,10 +4,22 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+let friends = [`Nitin`, `Eric`, `Jeddy`, `Cameron`, `Riley`]
+
 app.get(`/api/users`, (req,res) => {
-    let friends = [`Nitin`, `Eric`, `Jeddy`, `Cameron`, `Riley`]
     res.status(200).send(friends)
 })
+
+app.post(`/api/users`, (req,res) => {
+    friends.push(req.body.name)
+    res.status(200).send(friends)
+})
+
+app.delete(`/api/users`, (req,res) => {
+    friends.splice((friends.length-1),1)
+    res.status(200).send(friends)
+})
+
 
 app.get(`/weather/:temperature`, (req,res) => {
     const phrase = `<h3>It was ${req.params.temperature} today</h3>`
